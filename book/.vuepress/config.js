d299999b62ebbd456c57de184376fd16d6ade23b
wolf-tungsten <canis.tungsten@foxmail.com>
2020-04-17 23:25:12 +0800

    初步的模板

---
+++ book/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: '止于至善之外',
+    description: '东南大学电类专业保外经验手册',
+    themeConfig: {
+    sidebar:[
+        ['', '保外必经之路'], // 主页
+        '/作者简介',
+        '/学长学姐们的联系方式'
+        ]
+    }
+}
